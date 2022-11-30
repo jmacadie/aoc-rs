cc951378149bab1f787299b19aadeb384758c36e
James MacAdie (TT sandbox) <james.macadie@telerealtrillium.com>
2022-11-30 10:23:30 +0000

    Scaffolding for other years & days

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 pub mod common;
+pub mod year_2015;
+pub mod year_2016;
+pub mod year_2017;
+pub mod year_2018;
+pub mod year_2019;
 pub mod year_2020;
+pub mod year_2021;
+pub mod year_2022;
 
 use year_2020::day_10;
 
---
+++ src/year_2021/mod.rs
@@ -0,0 +1,26 @@
+pub mod day_01;
+pub mod day_02;
+pub mod day_03;
+pub mod day_04;
+pub mod day_05;
+pub mod day_06;
+pub mod day_07;
+pub mod day_08;
+pub mod day_09;
+pub mod day_10;
+pub mod day_11;
+pub mod day_12;
+pub mod day_13;
+pub mod day_14;
+pub mod day_15;
+pub mod day_16;
+pub mod day_17;
+pub mod day_18;
+pub mod day_19;
+pub mod day_20;
+pub mod day_21;
+pub mod day_22;
+pub mod day_23;
+pub mod day_24;
+pub mod day_25;
+
