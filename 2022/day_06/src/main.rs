19c4d0940fa98dcb6d21b63e33416a9d67089c31
James MacAdie <james@macadie.co.uk>
2022-12-06 08:04:06 +0000

    2022 Day 6
    Nice easy one today!

---
+++ 2022/day_06/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    let data = include_str!("input.txt");
+    println!("Part 1: {}", part_one(data));
+    println!("Part 2: {}", part_two(data));
+}
+
+fn part_one(data: &str) -> usize {
+    data.lines().map(|l| find_window(l, 4)).sum()
+}
+
+fn part_two(data: &str) -> usize {
+    data.lines().map(|l| find_window(l, 14)).sum()
+}
+
+fn find_window(line: &str, len: usize) -> usize {
+    for (i, window) in line.as_bytes().windows(len).enumerate() {
+        if unique(window) {
+            return i + len;
+        }
+    }
+    0
+}
+
+fn unique(letters: &[u8]) -> bool {
+    for i in 0..letters.len() - 1 {
+        let (_, rest) = letters.split_at(i);
+        if let Some((first, rest)) = rest.split_first() {
+            if rest.contains(first) {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn one() {
+        let data = include_str!("test.txt");
+        assert_eq!(7 + 5 + 6 + 10 + 11, part_one(data));
+    }
+
+    #[test]
+    fn two() {
+        let data = include_str!("test.txt");
+        assert_eq!(19 + 23 + 23 + 29 + 26, part_two(data));
+    }
+}
