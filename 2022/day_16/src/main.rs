543a07f690f354fa87e28398c3ab7220ce8f7e6a
James MacAdie <james@macadie.co.uk>
2022-12-18 01:04:59 +0000

    Day 16, Part 2
    Works on the test case but not the actual input???
    
    Obvs missing comething but I think I've refactored it down to a form I
    don't hate, so saving where I'm up to

--- 2022/day_16/src/main.rs
+++ 2022/day_16/src/main.rs
@@ -4,66 +4,87 @@ use std::collections::{HashMap, HashSet};
 pub fn main() {
     let data = include_str!("input.txt");
     let (flows, map) = parse_input(data);
+    println!("{flows:?}");
     println!("Part 1: {}", part_one(&flows, &map));
-    println!("Part 2: {}", part_two(data));
+    println!("Part 2: {}", part_two(&flows, &map));
 }
 
 fn part_one(flows: &Flows, map: &Map) -> u32 {
-    let mut curr_path = Vec::with_capacity(20);
-    curr_path.push("AA");
-    let (_path, value) = solve(&curr_path, 0, 30, flows, map, 0).unwrap();
-    //println!("{path:?}");
+    let mut human_path = Vec::with_capacity(20);
+    let mut elephant_path = Vec::with_capacity(20);
+    human_path.push("AA");
+    elephant_path.push("AA");
+    let (paths, value) = solve([&human_path, &elephant_path], [30, 0], 0, flows, map, 0).unwrap();
+    println!("{paths:?}");
+    value
+}
+
+fn part_two(flows: &Flows, map: &Map) -> u32 {
+    let mut human_path = Vec::with_capacity(20);
+    let mut elephant_path = Vec::with_capacity(20);
+    human_path.push("AA");
+    elephant_path.push("AA");
+    let (paths, value) = solve([&human_path, &elephant_path], [26, 26], 0, flows, map, 0).unwrap();
+    println!("{paths:?}");
     value
 }
 
 fn solve(
-    path: &[&'static str],
+    paths: [&[&'static str]; 2],
+    time: [u32; 2],
     value: u32,
-    time: u32,
     flows: &Flows,
     map: &Map,
     mut best: u32,
-) -> Option<(Vec<&'static str>, u32)> {
-    let mut path = path.to_vec();
-    let mut priority_list = get_priority_list(&path, time, flows, map);
+) -> Option<([Path; 2], u32)> {
+    if time[1] > time[0] {
+        let flipped_paths = [paths[1], paths[0]];
+        let flipped_time = [time[1], time[0]];
+        return solve(flipped_paths, flipped_time, value, flows, map, best);
+    }
+    let mut path = paths[0].to_vec();
     let mut out = None;
+    let mut priority_list = get_priority_list(paths, time[0], flows, map);
     while let Some((node, new_time, opportunity)) = priority_list.pop() {
         path.push(node);
         let value = value + opportunity;
-        if priority_list.is_empty() && value > best {
-            return Some((path, value));
+        if (priority_list.is_empty() || (new_time + time[1] == 0)) && value > best {
+            return Some(([path, paths[1].to_vec()], value));
         }
-        if value + remaining_opportunity(&path, new_time, flows, map) <= best {
+        let updated_paths = [&path, paths[1]];
+        let max_time = std::cmp::max(new_time, time[1]);
+        if value + remaining_opportunity(updated_paths, max_time, flows, map) <= best {
             path.pop();
             continue;
         }
-        if let Some(t) = solve(&path, value, new_time, flows, map, best) {
-            best = t.1;
-            out = Some(t);
+        let updated_time = [new_time, time[1]];
+        if let Some((p, v)) = solve(updated_paths, updated_time, value, flows, map, best) {
+            best = v;
+            out = Some((p, v));
         }
         path.pop(); // Take this item off to try the next
     }
     out
 }
 
-fn remaining_opportunity(path: &[&'static str], time: u32, flows: &Flows, map: &Map) -> u32 {
-    get_priority_list(path, time, flows, map)
+fn remaining_opportunity(paths: [&[&'static str]; 2], time: u32, flows: &Flows, map: &Map) -> u32 {
+    get_priority_list(paths, time, flows, map)
         .into_iter()
         .map(|(_, _, v)| v)
         .sum()
 }
 
 fn get_priority_list(
-    path: &[&'static str],
+    paths: [&[&'static str]; 2],
     time: u32,
     flows: &Flows,
     map: &Map,
-) -> Vec<(&'static str, u32, u32)> {
-    let node = path.last().unwrap();
+) -> Priorities {
+    let &node = paths[0].last().unwrap();
     map.get(node)
         .unwrap()
         .iter()
-        .filter(|(valve, _)| !path.contains(valve))
+        .filter(|(valve, _)| !paths.into_iter().flatten().contains(valve))
         .map(|(&valve, &dist)| {
             let rem = if time == 0 || dist > time - 1 {
                 0
@@ -72,17 +93,16 @@ fn get_priority_list(
             };
             (valve, rem, rem * flows.get(valve).unwrap())
         })
+        //.filter(|&(_, _, opportunity)| opportunity > 0)
         .sorted_by_key(|&(_, _, opportunity)| opportunity)
         .collect_vec()
-}
-
-fn part_two(_data: &str) -> usize {
-    0
 }
 
 type Flows = HashMap<&'static str, u32>;
 type UnitMap = HashMap<&'static str, Vec<&'static str>>;
 type Map = HashMap<&'static str, HashMap<&'static str, u32>>;
+type Priorities = Vec<(&'static str, u32, u32)>;
+type Path = Vec<&'static str>;
 
 fn parse_input(data: &'static str) -> (Flows, Map) {
     let mut flows = HashMap::with_capacity(20);
@@ -161,6 +181,7 @@ mod tests {
     #[test]
     fn two() {
         let data = include_str!("test.txt");
-        assert_eq!(0, part_two(data));
+        let (flows, map) = parse_input(data);
+        assert_eq!(1707, part_two(&flows, &map));
     }
 }
