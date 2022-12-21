1ae3a1f2393b0f26fb7e8454f8846d622940267c
James MacAdie <james@macadie.co.uk>
2022-12-21 01:25:20 +0000

    Day 20, Part 2
    Now done

--- 2022/day_20/src/main.rs
+++ 2022/day_20/src/main.rs
@@ -4,32 +4,38 @@ pub fn main() {
     println!("Part 2: {}", part_two::<5_000>(data));
 }
 
-fn part_one<const N: usize>(data: &str) -> i32 {
+fn part_one<const N: usize>(data: &str) -> i64 {
     let (mut nums, mut reference) = read_file::<N>(data);
     mix::<N>(&mut nums, &mut reference);
-    let index = find_index(&nums, 0);
+    get_coords::<N>(&nums)
+}
+
+fn part_two<const N: usize>(data: &str) -> i64 {
+    const KEY: i64 = 811_589_153;
+    let (mut nums, mut reference) = read_file::<N>(data);
+    for val in nums.iter_mut() {
+        *val *= KEY;
+    }
+    for _ in 0..10 {
+        mix::<N>(&mut nums, &mut reference);
+    }
+    get_coords::<N>(&nums)
+}
+
+fn get_coords<const N: usize>(nums: &[i64]) -> i64 {
+    let index = find_index(nums, 0);
     let x = nums[(1_000 + index) % N];
     let y = nums[(2_000 + index) % N];
     let z = nums[(3_000 + index) % N];
     x + y + z
 }
 
-fn part_two<const N: usize>(data: &str) -> i32 {
-    let (mut nums, mut reference) = read_file::<N>(data);
-    mix::<N>(&mut nums, &mut reference);
-    let index = find_index(&nums, 0);
-    let x = nums[(1_000 + index) % N];
-    let y = nums[(2_000 + index) % N];
-    let z = nums[(3_000 + index) % N];
-    x + y + z
-}
-
-fn mix<const N: usize>(nums: &mut [i32], reference: &mut [usize]) {
+fn mix<const N: usize>(nums: &mut [i64], reference: &mut [usize]) {
     for i in 0..N {
         let index = find_index(reference, i);
         let move_by = nums[index];
         move_array::<usize, N>(reference, index, move_by);
-        move_array::<i32, N>(nums, index, move_by);
+        move_array::<i64, N>(nums, index, move_by);
     }
 }
 
@@ -42,9 +48,9 @@ fn find_index<T: PartialEq>(array: &[T], target: T) -> usize {
     0
 }
 
-fn move_array<T: Copy, const N: usize>(array: &mut [T], from: usize, by: i32) {
-    let lim = i32::try_from(N).unwrap() - 1;
-    let mut to = (i32::try_from(from).unwrap() + by) % lim;
+fn move_array<T: Copy, const N: usize>(array: &mut [T], from: usize, by: i64) {
+    let lim = i64::try_from(N).unwrap() - 1;
+    let mut to = (i64::try_from(from).unwrap() + by) % lim;
     if to < 0 {
         to += lim;
     }
@@ -65,7 +71,7 @@ fn move_array<T: Copy, const N: usize>(array: &mut [T], from: usize, by: i32) {
     array[to] = moved;
 }
 
-fn read_file<const N: usize>(data: &str) -> ([i32; N], [usize; N]) {
+fn read_file<const N: usize>(data: &str) -> ([i64; N], [usize; N]) {
     let mut nums = [0; N];
     let mut reference = [0; N];
     for (i, line) in data.lines().enumerate() {
@@ -89,6 +95,6 @@ mod tests {
     #[test]
     fn two() {
         let data = include_str!("test.txt");
-        assert_eq!(0, part_two::<7>(data));
+        assert_eq!(1_623_178_306, part_two::<7>(data));
     }
 }
