3df676679fd5220ed6431de5ec00ab91ba56b8bf
James MacAdie (TT sandbox) <james.macadie@telerealtrillium.com>
2023-02-28 08:16:51 +0000

    Tidy up
    Small refactor of the inputs script to make it a bit more streamlined

--- inputs/src/main.rs
+++ inputs/src/main.rs
@@ -8,16 +8,14 @@ use std::io::Write;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let headers = get_headers();
-    if headers.is_err() {
+    let Ok(headers) = headers() else {
         println!("Missing the environment variable: 'AOC_SESSION'");
         println!("Cannot download personal inputs without it. Please extract it from a current web session.");
         println!("The session can be set (in bash/zsh) with:");
         println!("export AOC_SESSION=x");
         println!("Replace the x with your actual session value");
         return Ok(());
-    }
-    let headers = headers.unwrap();
+    };
 
     let client = reqwest::blocking::Client::builder()
         .default_headers(headers)
@@ -34,33 +32,30 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn process_day(year: u16, day: u8, client: &Client) -> Result<(), Box<dyn Error>> {
-    let path_str = format!("../{year}/day_{day:02}");
-    let path = PathBuf::from(&path_str);
-    if !path.exists() {
-        //println!("No folder for {year} day {day}. Skipping to next.");
+    let path = format!("../{year}/day_{day:02}");
+    if !PathBuf::from(&path).exists() {
+        // Quit out if we don't have the day folder
         return Ok(());
     }
 
-    let input_path = format!("{path_str}/src/input.txt");
-    let path = PathBuf::from(&input_path);
-    //println!("{} exists: {}", input_path, path.exists());
-    if !path.exists() {
+    let input_file = PathBuf::from(format!("{path}/src/input.txt"));
+    if !input_file.exists() {
         println!("Found missing data for {year}, day {day}. Downloading from website...");
-        let data = get_day_input(year, day, client)?;
-        let mut f = std::fs::File::create(path)?;
+        let data = day_input(year, day, client)?;
+        let mut f = std::fs::File::create(input_file)?;
         f.write_all(data.as_bytes())?;
     }
 
     Ok(())
 }
 
-fn get_day_input(year: u16, day: u8, client: &Client) -> reqwest::Result<String> {
+fn day_input(year: u16, day: u8, client: &Client) -> reqwest::Result<String> {
     let input_url = format!("https://adventofcode.com/{year}/day/{day}/input");
     let res = client.get(input_url).send()?;
     res.text()
 }
 
-fn get_headers() -> Result<header::HeaderMap, Box<dyn Error>> {
+fn headers() -> Result<header::HeaderMap, Box<dyn Error>> {
     let session = format!("session={}", env::var("AOC_SESSION")?);
     let mut headers = header::HeaderMap::new();
     let value = header::HeaderValue::from_str(&session)?;
