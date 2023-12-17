b6a0152a23ce8d7cf765129502eb57eb2a512e84
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2023-12-17 21:20:04 +0000

    2023 Day 16
    Lots of type state modelling today, but not too hard when you strip the
    boilerplate away.
    
    The tricky thing was to detect loops of beams, and terminate them before
    they created endless loops. I did this by maintaining a shadow array
    tracking what beams in what directions we had seen at each location. It
    was important to distinguish between directions, as a beam going over
    the same point in a different direction will be following a different
    path. I did this by using the first 4 bits of a u8 at each location.
    
    For part 2, I strongly suspect we can make it much faster still by using
    a dynamic programming approach and caching the ensuing path from each
    mirror / direction combination as we see it. The difficulty I had with
    that is that my endless loop detection and termination might be
    detecting an overlap with a bit of the path that predated the mirror
    and so the found path won't be generically applicable to future visits
    to that mirror from a different start location. An exercise for the
    future dear reader

--- src/days.rs
+++ src/days.rs
@@ -70,7 +70,7 @@ pub fn get(year: Year, day: usize) -> (fn(), &'static str) {
     }
 }
 
-pub const DAYS_2023: [(fn(), &str); 15] = [
+pub const DAYS_2023: [(fn(), &str); 16] = [
     (day_2023_01::main, "Day 1"),
     (day_2023_02::main, "Day 2"),
     (day_2023_03::main, "Day 3"),
@@ -86,7 +86,7 @@ pub const DAYS_2023: [(fn(), &str); 15] = [
     (day_2023_13::main, "Day 13"),
     (day_2023_14::main, "Day 14"),
     (day_2023_15::main, "Day 15"),
-    //     (day_2023_16::main, "Day 16"),
+    (day_2023_16::main, "Day 16"),
     //     (day_2023_17::main, "Day 17"),
     //     (day_2023_18::main, "Day 18"),
     //     (day_2023_19::main, "Day 19"),
