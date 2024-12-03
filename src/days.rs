431160b0c51e2e46915f6c08685846eb93acba78
James MacAdie <j.macadie@ttg.co.uk>
2024-12-03 08:54:38 +0000

    2024 Day 3
    Today was crying out for nom, but since I'm trying to be vanilla I coded my own.
    I went with a state machine to tell where we were up to in the parsing.
    
    It's coded up as two separate passes through the text currently, which I may
    choose to come back and refactor. If you're reading this though it looks like I
    didn't!

--- src/days.rs
+++ src/days.rs
@@ -75,10 +75,10 @@ pub fn get(year: Year, day: usize) -> (fn(), &'static str) {
     }
 }
 
-pub const DAYS_2024: [(fn(), &str); 2] = [
+pub const DAYS_2024: [(fn(), &str); 3] = [
     (day_2024_01::main, "Day 1"),
     (day_2024_02::main, "Day 2"),
-    // (day_2024_03::main, "Day 3"),
+    (day_2024_03::main, "Day 3"),
     // (day_2024_04::main, "Day 4"),
     // (day_2024_05::main, "Day 5"),
     // (day_2024_06::main, "Day 6"),
